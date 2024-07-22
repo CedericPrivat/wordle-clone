fa4db9a5a77cddb72e15b57cadcf97d58cc29b5c
Cederic Privat <cederic.privat@gmail.com>
2024-07-22 17:05:51 +0200

    feat: add GuessResults

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-function GuessInput() {
-  const [guess, setGuess] = React.useState('');
+function GuessInput({ handleSubmitGuess }) {
+  const [tentativeGuess, setTentativeGuess] = React.useState('');
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(guess);
-    setGuess('');
+    handleSubmitGuess(tentativeGuess);
+    setTentativeGuess('');
   }
 
   return (
@@ -16,8 +16,10 @@ function GuessInput() {
         id='guess-input'
         type='text'
         title='5 letter word'
-        value={guess}
-        onChange={(event) => setGuess(event.target.value.toUpperCase())}
+        value={tentativeGuess}
+        onChange={(event) =>
+          setTentativeGuess(event.target.value.toUpperCase())
+        }
         pattern='[a-zA-Z]{5}'
         required
       />
